e60d8713dec4da3189660ad2d9cbeb8c6088a1ed
Adrien <ajouary@gmail.com>
2024-11-24 16:23:44 +0000

    Add simulate for mDLAG

--- src/latents/mdlag/simulation.py
+++ src/latents/mdlag/simulation.py
@@ -10,25 +10,98 @@ Simulate data from the mDLAG generative model.
 """
 
 from __future__ import annotations
-from latents.observation_model.observations import ObsTimeSeries
-from latents.observation_model.probabilistic import ObsParamsARD
-from latents.state_model.latents import StateParamsGP
-from latents.state_model.GP_latents import construct_K_mdlag_fast
+
 import numpy as np
 
-def simulate():
-    """Generate samples from the full mDLAG model."""
-    pass
+from latents.observation_model.observations import ObsTimeSeries
+from latents.observation_model.probabilistic import (
+    HyperPriorParams,
+    ObsParamsARD,
+)
+from latents.state_model.latents import StateParamsGP
+from latents.state_model.GP_latents import construct_K_mdlag_fast
 
 
-def generate_latents(state_params: StateParamsGP, N: int, rng: np.random.Generator=np.random.default_rng()):
-    """Generate latents via the mDLAG state model.
-    
-    Generates N independent sequences
-    according to a zero-mean Gaussian Process defined by the mDLAG state model.
+def simulate(
+    N: int,
+    y_dims: np.ndarray,
+    hyper_priors: HyperPriorParams,
+    state_params: StateParamsGP,
+    snr: np.ndarray,
+    random_seed: int | None = None,
+) -> tuple[ObsTimeSeries, np.ndarray, ObsParamsARD]:
+    """
+    Generate samples from the full mDLAG model.
+
+    Parameters
+    ----------
+    N
+        Number of sequences to generate.
+    y_dims
+        `ndarray` of `int`, shape ``(num_groups,)``.
+        Number of observed dimensions in each group.
+    hyper_priors
+        Hyperparameters of the observation model priors.
+    state_params
+        Parameters of the state model.
+    snr
+        `ndarray` of `float`, shape ``(num_groups,)``.
+        Signal-to-noise ratio of each group.
+    random_seed
+        Random seed for reproducibility.
+
     Returns
     -------
-        latent tensor (N, x_dim, num_groups, T)         
+    ObsTimeSeries
+        Generated observed data.
+    ndarray
+        Generated latent data, shape ``(x_dim, num_groups, T, N)``.
+    ObsParamsARD
+        Generated observation model parameters.
+    """
+    rng = np.random.default_rng(seed=random_seed)
+    x_dim = state_params.x_dim
+
+    # Generate observation model parameters
+    obs_params = ObsParamsARD.generate(y_dims, x_dim, hyper_priors, snr, rng)
+
+    # Generate latent variables
+    latents = generate_latents(state_params, N, rng)
+    X = np.transpose(latents, (1, 2, 3, 0))
+
+    # Generate observations
+    Y = generate_observations(X, obs_params, rng)
+
+    return Y, X, obs_params
+
+
+def generate_latents(
+    state_params: StateParamsGP,
+    N: int,
+    rng: np.random.Generator = np.random.default_rng(),
+) -> np.ndarray:
+    """
+    Generate latents via the mDLAG state model.
+
+    Parameters
+    ----------
+    state_params
+        Parameters of the state model.
+    N
+        Number of sequences to generate.
+    rng
+        A random number generator object.
+
+    Returns
+    -------
+    ndarray
+        `ndarray` of `float`, shape ``(N, x_dim, num_groups, T)``.
+        Latent data.
+
+    Raises
+    ------
+    ValueError
+        If the covariance matrix is not positive-definite.
     """
     num_groups = state_params.num_groups
     x_dim = state_params.x_dim
@@ -46,27 +119,24 @@ def generate_latents(state_params: StateParamsGP, N: int, rng: np.random.Generat
         raise ValueError("Covariance matrix is not positive-definite.")
 
     mean = np.zeros(K_big_size)
-    #xsm = np.zeros((N,num_groups*x_dim, T))
-    latents = np.zeros((N,x_dim,num_groups,T))
-    # x_dim, num_groups, T,
+    latents = np.zeros((N, x_dim, num_groups, T))
+
     for n in range(N):
         # Sample latent variables
         u = rng.standard_normal(K_big_size)
         x_n = L @ u + mean
-
-        # Reshape to (numGroups * xDim, Tj)
-        #xsm[n, :, :] = x_n.reshape((num_groups * x_dim, T), order="F")
         latents[n, :, :, :] = x_n.reshape((x_dim, num_groups, T), order="F")
 
     return latents
 
 
 def generate_observations(
-        X: np.ndarray,
-        obs_params: ObsParamsARD,
-        rng: np.random.Generator,
-    ) -> ObsTimeSeries:
-    """Generate observations via the mDLAG observation model.
+    X: np.ndarray,
+    obs_params: ObsParamsARD,
+    rng: np.random.Generator,
+) -> ObsTimeSeries:
+    """
+    Generate observations via the mDLAG observation model.
 
     Parameters
     ----------
@@ -74,7 +144,7 @@ def generate_observations(
         `ndarray` of `float`, shape ``(x_dim, num_groups, T, N)``.
         Latent data.
     obs_params
-        GFA observation model parameters.
+        mDLAG observation model parameters.
     rng
         A random number generator object.
 
@@ -91,6 +161,7 @@ def generate_observations(
     num_groups = len(y_dims)
     # Number of time points
     T = X.shape[2]
+
     # Split d, phi, and C according to observed groups
     ds, _ = obs_params.d.get_groups(y_dims)
     phis, _ = obs_params.phi.get_groups(y_dims)
@@ -103,7 +174,7 @@ def generate_observations(
     # Generate observated data group by group
     for group_idx in range(num_groups):
         Ys[group_idx][:] = (
-            np.einsum('ij,j...->i...', Cs[group_idx], X[:, group_idx, :, :])
+            np.einsum("ij,j...->i...", Cs[group_idx], X[:, group_idx, :, :])
             + ds[group_idx][:, None, None]
             + rng.multivariate_normal(
                 np.zeros(y_dims[group_idx]), 
@@ -111,5 +182,5 @@ def generate_observations(
                 size=(T, N)
             ).transpose(2, 0, 1)
         )
-        
+
     return Y
