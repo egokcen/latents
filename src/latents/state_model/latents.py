13fcedd43be42b0ae19f2c224991c74ac5136a32
Adrien <ajouary@gmail.com>
2024-12-12 19:42:28 +0000

    PR Review: remove reference to gp parameters from latents.py

--- src/latents/state_model/latents.py
+++ src/latents/state_model/latents.py
@@ -14,7 +14,6 @@ from __future__ import annotations
 import numpy as np
 
 from latents.base import ArrayContainer
-from latents.state_model.GP_latents import RBF_GP_Params
 
 
 class PosteriorLatentStatic(ArrayContainer):
@@ -145,9 +144,107 @@ class PosteriorLatentStatic(ArrayContainer):
 
 
 class PosteriorLatentDelayed(ArrayContainer):
-    """Posterior estimates of time-delayed latent variables."""
+    """Posterior estimates of time-delayed latent variables.
 
-    pass
+    Parameters
+    ----------
+    mean
+        `ndarray` of `float`, shape ``(x_dim, num_groups, T, N)``.
+        Posterior mean of the latent variables.
+    cov
+        `ndarray` of `float`, shape ``(x_dim, num_groups, T, x_dim, num_groups, T)``.
+        Posterior covariance of the latent variables.
+    moment
+        `ndarray` of `float`, shape ``(x_dim, num_groups, T, x_dim, num_groups, T)``.
+        Posterior second moments of the latent variables.
+    moment_GP
+        `ndarray` of `float`, shape ``(x_dim, num_groups, T, x_dim, num_groups, T)``.
+        Posterior second moments of the latent variables for the GP case.
+
+    Attributes
+    ----------
+    mean
+        Same as **mean**, above.
+    cov
+        Same as **cov**, above.
+    moment
+        Same as **moment**, above.
+
+    Raises
+    ------
+    TypeError
+        If ``mean``, ``cov``, or ``moment`` is not a `ndarray`.
+    """
+
+    def __init__(
+        self,
+        mean: np.ndarray | None = None,
+        cov: np.ndarray | None = None,
+        moment: np.ndarray | None = None,
+    ):
+        # Mean
+        if mean is not None and not isinstance(mean, np.ndarray):
+            msg = "mean must be a numpy.ndarray."
+            raise TypeError(msg)
+        self.mean = mean
+
+        # Covariance
+        if cov is not None and not isinstance(cov, np.ndarray):
+            msg = "cov must be a numpy.ndarray."
+            raise TypeError(msg)
+        self.cov = cov
+
+        # Second moment
+        if moment is not None and not isinstance(moment, np.ndarray):
+            msg = "moment must be a numpy.ndarray."
+            raise TypeError(msg)
+        self.moment = moment
+
+    def compute_moment_GP(self, in_place: bool = True) -> np.ndarray | None:
+        """
+        Compute the posterior second moments of the latent variables for GP case.
+
+        This function is written to be differentiable with respect to the parameters x.
+
+        Parameters
+        ----------
+        in_place
+            If ``True``, compute the posterior second moments in place.
+            If ``False``, compute the posterior second moments and return as a
+            new `ndarray` without modifying self. Defaults to ``True``.
+
+        Returns
+        -------
+        ndarray | None
+            `ndarray` of shape ``(x_dim, num_groups*T, num_groups*T, N)``.
+            Posterior second moments of the latent variables.
+        """
+        x_dim, num_groups, T, N = self.mean.shape
+        moment_GP = np.zeros((x_dim, num_groups * T, num_groups * T, N))
+
+        for j in range(x_dim):
+            for n in range(N):
+                Sig_jj = self.cov[j, :, :, j, :, :].reshape(
+                    num_groups * T, num_groups * T, order="F"
+                )
+                mu_j = self.mean[j, :, :, n].reshape(num_groups * T, 1, order="F")
+                moment_GP[j, :, :, n] = Sig_jj + mu_j @ mu_j.T
+
+        if in_place:
+            self.moment = moment_GP
+            return None
+
+        return moment_GP
+
+    def compute_moment(self, in_place: bool = True) -> np.ndarray | None:
+        """Compute the posterior second moments of the latent variables."""
+        raise NotImplementedError
+
+    def get_subset_dims(
+        self, dims: np.ndarray, in_place: bool = True
+    ) -> PosteriorLatentDelayed | None:
+        """Keep only a subset of the latent dimensions in each attribute."""
+        raise NotImplementedError
 
 
 class StateParamsStatic:
@@ -261,7 +358,7 @@ class StateParamsStatic:
         )
 
 
-class StateParamsGP:
+class StateParamsDelayed:
     """A Gaussian Process state model."""
 
     def __init__(
@@ -270,7 +367,6 @@ class StateParamsGP:
         num_groups: int | None = None,
         T: int | None = None,
         X: PosteriorLatentDelayed | None = None,
-        gp_params: RBF_GP_Params | None = None,
     ):
         # Latent dimensionality
         if x_dim is not None and not isinstance(x_dim, int):
@@ -299,14 +395,6 @@ class StateParamsGP:
             raise TypeError(msg)
         self.T = T
 
-        # Kernel parameters
-        if gp_params is None and all(p is not None for p in [x_dim, num_groups, T]):
-            self.gp_params = RBF_GP_Params(
-                x_dim=x_dim, num_groups=num_groups, T=T, eps=None, gamma=None, D=None
-            )
-        else:
-            self.gp_params = gp_params
-
     def __repr__(self) -> str:
         return (
             f"{type(self).__name__}("
@@ -314,7 +402,6 @@ class StateParamsGP:
             f"num_groups={self.num_groups}, "
             f"T={self.T}, "
             f"X={self.X}, "
-            f"gp_params={self.gp_params})"
         )
 
     def is_initialized(self) -> bool:
@@ -323,10 +410,10 @@ class StateParamsGP:
 
     def get_subset_dims(
         self, dims: np.ndarray, in_place: bool = True
-    ) -> StateParamsGP | None:
+    ) -> StateParamsDelayed | None:
         """Keep only a subset of the latent dimensions in each relevant parameter."""
         raise NotImplementedError
 
-    def copy(self) -> StateParamsGP:
+    def copy(self) -> StateParamsDelayed:
         """Return a copy of self."""
         raise NotImplementedError
